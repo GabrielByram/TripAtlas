e01a1f8d6736bf09e8acf3c86c4cf1cacf4daf36
GabrielByram <60529017+GabrielByram@users.noreply.github.com>
2023-05-29 14:54:02 -0400

    Added login and sign up front-end merged together

--- Trip Atlas/Code/client/src/Pages/LoginPage.tsx
+++ Trip Atlas/Code/client/src/Pages/LoginPage.tsx
@@ -1,11 +1,87 @@
-import Button from 'react-bootstrap/Button';
+import "./LoginPage.css"
+import { useState } from 'react';
+import { Container, Form, Button, ButtonGroup, Row, Col } from 'react-bootstrap';
 
-function LoginPage() {
+const LoginPage = () => {
+  const [email, setEmail] = useState('');
+  const [password, setPassword] = useState('');
+  const [showPassword, setShowPassword] = useState(false);
+  const [isNewUser, setIsNewUser] = useState(false);
+
+  const handleContinue = () => {
+    // Perform email validation and check if it exists in the database
+    // Set isNewUser based on whether the email is found in the database
+    setIsNewUser(true); // For demonstration purposes, set isNewUser to true
+  };
+
+  const handleLogin = () => {
+    // Perform login with the email and password
+    // Redirect or perform necessary actions after successful login
+  };
+
+  const handleCreateAccount = () => {
+    // Perform account creation with the email and password
+    // Redirect or perform necessary actions after successful account creation
+  };
+
   return (
-    <Button>
-      Login Page
-    </Button>
-  );
-}
+    <Container className="d-flex flex-column align-items-center mt-5">
+      <h1 className="text-center">LOGIN</h1>
+      <Form className="w-50">
+        <Form.Group controlId="formEmail">
+          <Form.Label>Email address</Form.Label>
+          <Form.Control
+            type="email"
+            placeholder="Enter email"
+            value={email}
+            onChange={(e) => setEmail(e.target.value)}
+          />
+        </Form.Group>
+        {!isNewUser ? (
+          <div>
+            <Button className="login-button" variant="primary" onClick={handleContinue}>
+              Continue
+            </Button>
+            <div className="text-center mt-3">
+              <p>Or log in with:</p>
+              <Button variant="outline-primary">Google Account</Button>
+            </div>
+            <div className="text-center mt-3">
+              <p>Not a member? Sign up here</p>
+            </div>
+          </div>
+        ) : (
+          <div>
+            <div style={{marginTop: "15px"}}>
+              <Form.Group controlId="formPassword">
+                <Form.Label>Password</Form.Label>
+                <Form.Control
+                  type={showPassword ? 'text' : 'password'}
+                  placeholder="Password"
+                  value={password}
+                  onChange={(e) => setPassword(e.target.value)}
+                />
 
-export default LoginPage;
+              </Form.Group>
+              <Form.Group controlId="formRememberMe">
+                <div className="d-flex justify-content-between">
+                    <Form.Check type="checkbox" label="Remember me" />
+                    <Button variant="link" style={{float: "right", padding: "0", marginTop: "-5px"}}>
+                      Forgot Password?
+                    </Button>
+                </div>
+              </Form.Group>
+            </div>
+            <div>
+              <Button variant="primary" onClick={handleLogin} className="login-button">
+                Login
+              </Button>
+            </div>
+          </div>
+        )}
+      </Form>
+    </Container>
+  );
+};
+
+export default LoginPage
